8efdfdfaaf46e0da2cbe6999511d281f686a91c1
GTL <gaotianliang@hrbeu.edu.cn>
2019-05-06 18:05:56 +0800

    1.4 导入模块，完成cards_main 中的所有代码

--- cards_main.py
+++ cards_main.py
@@ -4,6 +4,9 @@
 程序的入口
 每一次启动名片管理系统都通过 main 这个文件启动 
 """
+#在 cards_main.py 中使用 import 导入 cards_tools 模块
+import cards_tools
+
 
 #添加一个 无限循环
 # 无限循环
@@ -14,6 +17,7 @@ while True:
     
     # TODO(小明) 显示系统菜单
     # 注释TODO ：在 # 后跟上TODO，用于标记需要去做的工作
+    cards_tools.show_menu()
 
     action = input("请选择操作功能：")
 
@@ -35,7 +39,15 @@ while True:
         使用 in 针对 列表 判断，避免使用 or 拼接复杂的逻辑条件
         没有使用 int 转换用户输入，可以避免 一旦用户输入的不是数字，导致程序运行出错
         '''
+        if action == "1":
+            cards_tools.new_card()
 
+        elif action == "2":
+            cards_tools.show_all()
+
+        elif action == "3":
+            cards_tools.search_card()
+            
     elif action == "0":
         print("欢迎再次使用【名片管理系统】")
 
