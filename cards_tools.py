9252e520f0efa28989b36449228a29067195c75c
GTL <gaotianliang@hrbeu.edu.cn>
2019-05-06 18:08:06 +0800

    1.5 完成 show_menu 函数

--- cards_tools.py
+++ cards_tools.py
@@ -8,7 +8,16 @@ def show_menu():
 
     """显示菜单
     """
-    pass
+    
+    print("*" * 50)
+    print("欢迎使用【菜单管理系统】V1.0")
+    print("")
+    print("1. 新建名片")
+    print("2. 显示全部")
+    print("3. 查询名片")
+    print("")
+    print("0. 退出系统")
+    print("*" * 50)
 
 def new_card():
 
